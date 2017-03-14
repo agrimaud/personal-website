ee190061e18d3270d42eae90e8906a1766b74c96
Alban Grimaud <alban@Albans-MacBook-Pro.local>
2017-03-14 16:42:27 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,89 @@
+$(document).ready(
+  function(){
+
+    //hover on expertise-item in h3;
+
+    $('#item1').hover(
+      function() {
+          $('#detail2').css('opacity','0.3');
+          $('#detail3').css('opacity','0.3');
+      }
+    );
+    $('#item1').mouseleave(
+      function() {
+        $('#detail2').css('opacity','1');
+        $('#detail3').css('opacity','1');
+      }
+    );
+
+    $('#item2').hover(
+      function() {
+          $('#detail1').css('opacity','0.3');
+          $('#detail3').css('opacity','0.3');
+      }
+    );
+    $('#item2').mouseleave(
+      function() {
+        $('#detail1').css('opacity','1');
+        $('#detail3').css('opacity','1');
+      }
+    );
+
+    $('#item3').hover(
+      function() {
+          $('#detail1').css('opacity','0.3');
+          $('#detail2').css('opacity','0.3');
+      }
+    );
+    $('#item3').mouseleave(
+      function() {
+        $('#detail1').css('opacity','1');
+        $('#detail2').css('opacity','1');
+      }
+    );
+
+    //hover on expertise block itself;
+
+    $('#detail1').hover(
+      function() {
+          $('#detail2').css('opacity','0.3');
+          $('#detail3').css('opacity','0.3');
+      }
+    );
+    $('#detail1').mouseleave(
+      function() {
+        $('#detail2').css('opacity','1');
+        $('#detail3').css('opacity','1');
+      }
+    );
+
+    $('#detail2').hover(
+      function() {
+          $('#detail1').css('opacity','0.3');
+          $('#detail3').css('opacity','0.3');
+      }
+    );
+    $('#detail2').mouseleave(
+      function() {
+        $('#detail1').css('opacity','1');
+        $('#detail3').css('opacity','1');
+      }
+    );
+
+    $('#detail3').hover(
+      function() {
+          $('#detail1').css('opacity','0.3');
+          $('#detail2').css('opacity','0.3');
+      }
+    );
+    $('#detail3').mouseleave(
+      function() {
+        $('#detail1').css('opacity','1');
+        $('#detail2').css('opacity','1');
+      }
+    );
+
+
+
+
+});
